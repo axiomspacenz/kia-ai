4ab58c1038c61e783951d1dae4aa5d88d9a28a4b
axiomspacenz <admin@axiomspace.co.nz>
2024-04-26 09:25:11 +1200

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,9 @@ import type { Metadata } from "next";
 import { IBM_Plex_Sans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
-import { ClerkProvider } from "@clerk/nextjs";
+import {
+  ClerkProvider,
+} from "@clerk/nextjs";
 
 const IBMPlex = IBM_Plex_Sans({
   subsets: ["latin"],
@@ -21,7 +23,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider appearance={{
+      variables: { colorPrimary: "#624cf5" }
+    }}>
       <html lang="en">
         <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
           {children}
