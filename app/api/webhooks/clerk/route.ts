c67c018b39e0de6a211aa113cf32c54c00e145b6
axiomspacenz <admin@axiomspace.co.nz>
2024-04-27 09:53:13 +1200

    Webhook Debugging

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -11,6 +11,9 @@ export async function POST(req: Request) {
     // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
     const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
 
+    console.log("WEBHOOK REQUEST: ", req)
+    console.log("WEBHOOK SECRET: ", WEBHOOK_SECRET)
+
     if (!WEBHOOK_SECRET) {
         throw new Error(
             "Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local"
@@ -22,6 +25,10 @@ export async function POST(req: Request) {
     const svix_id = headerPayload.get("svix-id");
     const svix_timestamp = headerPayload.get("svix-timestamp");
     const svix_signature = headerPayload.get("svix-signature");
+
+    console.log("svix_id")
+    console.log("svix_timestamp")
+    console.log("svix_signature")
 
     // If there are no headers, error out
     if (!svix_id || !svix_timestamp || !svix_signature) {
@@ -57,6 +64,8 @@ export async function POST(req: Request) {
     const { id } = evt.data;
     const eventType = evt.type;
 
+    console.log("EVENT TYPE: ", eventType)
+
     // CREATE
     if (eventType === "user.created") {
         const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
@@ -70,7 +79,11 @@ export async function POST(req: Request) {
             photo: image_url,
         };
 
+        console.log("CREATED USER: ", user)
+
         const newUser = await createUser(user);
+
+        console.log("CREATED NEW USER: ", newUser)
 
         // Set public metadata
         if (newUser) {
